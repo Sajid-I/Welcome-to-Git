9c0215ffaadb59c680df50369e2bc1b923b39800
Sajid-I <sajidulislam1003.com@gmail.com>
2024-10-17 19:51:35 +0600

    None is loading

--- provide/test.js
+++ provide/test.js
@@ -2,6 +2,7 @@ const accountId = 199340
 let accountEmail = "sajidulsejam@gmail.com"
 var accountPassword = "239404982"
 accountCity = "Barishal"
+let accountState;
 
 //accountId = 2 // not allowed
 
@@ -9,5 +10,9 @@ accountEmail = "gghfj@gmail.com"
 accountPassword = "2938409"
 accountCity = "Dhaka"
 
+/* prefer not to use var because of issue in block scope 
+and functional scope 
+*/
 console.log(accountId);
-console.table([accountEmail,accountPassword,accountCity])
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
