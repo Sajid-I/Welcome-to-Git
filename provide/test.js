80d5bed883af904924d2498b951e2ba7178f095a
Sajid-I <sajidulislam1003.com@gmail.com>
2024-10-17 19:07:08 +0600

    littlle change

---
+++ provide/test.js
@@ -0,0 +1,13 @@
+const accountId = 199340
+let accountEmail = "sajidulsejam@gmail.com"
+var accountPassword = "239404982"
+accountCity = "Barishal"
+
+//accountId = 2 // not allowed
+
+accountEmail = "gghfj@gmail.com"
+accountPassword = "2938409"
+accountCity = "Dhaka"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
